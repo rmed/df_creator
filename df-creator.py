0505ee7856ebad6a7fcfb643f49e2f00db39c791
RMed <rafamedgar@gmail.com>
2013-02-04 21:16:11 +0100

    Implemented save function

--- df-creator.py
+++ df-creator.py
@@ -22,8 +22,47 @@ class MainGui:
         self.builder.get_object("switch_terminal").set_active(False)
         
     def on_save_click(self, widget):
-        print "Yet to implement"
+        savedialog = Gtk.FileChooserDialog("Save file", self.win, Gtk.FileChooserAction.SAVE, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
+
+        dfilter = Gtk.FileFilter()
+        dfilter.set_name(".desktop files")
+        dfilter.add_mime_type("application/x-desktop")
+        savedialog.add_filter(dfilter)
+
+        response = savedialog.run()
+        if response == Gtk.ResponseType.OK:
+            #print savedialog.get_filename()
+            self.save_desktop_file(savedialog.get_filename())
+
+        savedialog.destroy()
         
+    def save_desktop_file(self, filepath):
+        # Get values from GUI fields
+        name = self.builder.get_object("entry_name").get_text()
+        command = self.builder.get_object("entry_command").get_text()
+        icon = self.builder.get_object("entry_icon").get_text()
+        apptype = self.builder.get_object("entry_type").get_text()
+
+        category_index = self.builder.get_object("cbox_category").get_active()
+        category_model = self.builder.get_object("cbox_category").get_model()
+        category = category_model[category_index][0]
+
+
+        useterm = "false"
+        if self.builder.get_object("switch_terminal").get_active():
+            useterm = "true"
+
+        # Define the contents of the file
+        contents = ["[Desktop Entry]\n", "Name=" + name + "\n", "Exec=" + command + "\n", "Icon=" + icon + "\n", "Type=" + apptype + "\n", "Terminal=" + useterm + "\n", "Categories=" + category + ";"]
+
+        try:
+            dfile = open(filepath, "w")
+            try:
+                dfile.writelines(contents)
+            finally:
+                dfile.close()
+        except IOError:
+            pass 
         
 window = MainGui()
 Gtk.main()
