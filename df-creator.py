68659382109959658f6f6326351de7254a6c2135
RMed <rafamedgar@gmail.com>
2013-03-13 11:18:12 +0100

    Added about dialog. First stable version

--- df-creator.py
+++ df-creator.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python
+'''
+Main program controller
+'''
 
 from gi.repository import Gtk
 from functions import *
@@ -20,24 +22,122 @@ class MainGui:
     def on_delete_window(self, *args):
         Gtk.main_quit(*args)
 
+    # Search for element to execute
+    def on_btn_exec_clicked(self, widget):
+
+        # Create dialog for choosing element
+        opendialog = Gtk.FileChooserDialog("Choose element", self.win, Gtk.FileChooserAction.OPEN, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+        response = opendialog.run()
+        # Change exec
+        if response == Gtk.ResponseType.OK:
+            self.builder.get_object("entry_exec").set_text(opendialog.get_filename())
+
+        # Destroy dialog
+        opendialog.destroy()
+
+    # Search for an icon
+    def on_btn_icon_clicked(self, widget):
+
+        # Create dialog for choosing icon
+        opendialog = Gtk.FileChooserDialog("Choose icon", self.win, Gtk.FileChooserAction.OPEN, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+        # Add filters
+        ifilter = Gtk.FileFilter()
+        ifilter.set_name("Image files")
+        ifilter.add_mime_type("image/*")
+        opendialog.add_filter(ifilter)
+
+        response = opendialog.run()
+        # Change icon
+        if response == Gtk.ResponseType.OK:
+            self.builder.get_object("entry_icon").set_text(opendialog.get_filename())
+
+        # Destroy dialog
+        opendialog.destroy()
+
+    # Search for element to execute
+    def on_btn_tryexec_clicked(self, widget):
+
+        # Create dialog for choosing element
+        opendialog = Gtk.FileChooserDialog("Choose element", self.win, Gtk.FileChooserAction.OPEN, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+        response = opendialog.run()
+        # Change exec
+        if response == Gtk.ResponseType.OK:
+            self.builder.get_object("entry_tryexec").set_text(opendialog.get_filename())
+
+        # Destroy dialog
+        opendialog.destroy()
+
+    # Search for element to execute
+    def on_btn_path_clicked(self, widget):
+
+        # Create dialog for choosing directory
+        opendialog = Gtk.FileChooserDialog("Choose directory", self.win, Gtk.FileChooserAction.SELECT_FOLDER, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+        response = opendialog.run()
+        # Change exec
+        if response == Gtk.ResponseType.OK:
+            self.builder.get_object("entry_path").set_text(opendialog.get_filename())
+
+        # Destroy dialog
+        opendialog.destroy()
+
+
+    # About dialog
+    def on_about_activate(self, widget):
+
+        # Get the dialog from the glade file
+        dialog = self.builder.get_object("about_dialog")
+        response = dialog.run()
+
+        # Get response
+        if response == Gtk.ResponseType.CANCEL or response == Gtk.ResponseType.DELETE_EVENT:
+            # Hide the dialog
+            dialog.hide()
+
     # Clean all the fields and start from scratch
     def on_new_activate(self, widget):
-        entries = {
-            "entry_name", "entry_exec", "entry_icon", "entry_url", "entry_version", "entry_generic_name", "entry_comment", "entry_notshowin", "entry_tryexec", "entry_path", "entry_mime", "entry_keywords", "entry_wmclass"
-        }
-        for entry in entries:
-            self.builder.get_object(entry).set_text('')
 
-        self.builder.get_object("switch_terminal").set_active(False)
+        # Create dialog asking whether or not to continue
+        newdialog = Gtk.MessageDialog(self.win, Gtk.DialogFlags.DESTROY_WITH_PARENT, Gtk.MessageType.INFO, Gtk.ButtonsType.YES_NO, "All unsaved progress will be lost, continue?")
+
+        response = newdialog.run()
+        # Reset all the content
+        if response == Gtk.ResponseType.YES:
+            entries = {
+                "entry_name", "entry_exec", "entry_icon", "entry_url", "entry_version", "entry_generic_name", "entry_comment", "entry_notshowin", "entry_tryexec", "entry_path", "entry_mime", "entry_keywords", "entry_wmclass"
+            }
+            # Clear entries
+            for entry in entries:
+                self.builder.get_object(entry).set_text('')
+
+            # Clear switches
+            self.builder.get_object("switch_terminal").set_active(False)
+            self.builder.get_object("switch_nodisplay").set_active(False)
+            self.builder.get_object("switch_hidden").set_active(False)
+            self.builder.get_object("switch_notify").set_active(False)
+        
+        # Destroy Dialog
+        newdialog.destroy()
         
     # Save file
     def on_save_activate(self, widget):
+
         # Create save dialog
         savedialog = Gtk.FileChooserDialog("Save file", self.win, Gtk.FileChooserAction.SAVE, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
+
+        # Ask for confirmation on overwrite
+        savedialog.set_do_overwrite_confirmation(True) 
+
+        # Define initial name for the file
+        savedialog.set_current_name(self.builder.get_object("entry_name").get_text().lower())     
 
         # Add filter
         dfilter = Gtk.FileFilter()
         dfilter.set_name(".desktop")
+        dfilter.add_pattern("*.desktop")
         dfilter.add_mime_type("application/x-desktop")
         savedialog.add_filter(dfilter)
 
@@ -51,6 +151,7 @@ class MainGui:
 
     # Hide objects depending on the type selected
     def on_type_changed(self, widget):
+
         # Create lists of the boxes present in the window
         type_application_boxes = {
             "box_tryexec", "box_exec", "box_path", "box_terminal", "box_mime", "box_category", "box_keywords", "box_notify", "box_wmclass"
@@ -89,7 +190,8 @@ class MainGui:
             for box in type_application_boxes:
                 self.builder.get_object(box).set_visible(False)
             self.builder.get_object("box_url").set_visible(False)
-        
+
+# Create main window       
 window = MainGui()
 Gtk.main()
 
--- functions/save_file.py
+++ functions/save_file.py
@@ -29,52 +29,56 @@ def savedf(window, savepath):
     except IOError:
         pass 
 
+# Type Application
 def save_type_app(values):
     # Determine the final contents of the file
     final_content = ["[Desktop Entry]\n"]
-    # Type?
-    final_content.append("Type=Application\n")
-    # Has version?
-    if values.version != '':
-        final_content.append("Version=" + values.version + "\n")
     # Add name
     final_content.append("Name=" + values.name + "\n")
     # Has generic name?
     if values.genericname != '':
         final_content.append("GenericName=" + values.genericname + "\n")
-    # Must not display?
-    final_content.append("NoDisplay=" + values.nodisplay + "\n")
     # Has comment?
     if values.comment != '':
         final_content.append("Comment=" + values.comment + "\n")
-    # Has icon?
-    if values.icon != '':
-        final_content.append("Icon=" + values.icon + "\n")
-    # Is hidden?
-    final_content.append("Hidden=" + values.hidden + "\n")
-    # Must not show in some environment?
-    if values.notshowin != '':
-        final_content.append("NotShowIn=" + values.notshowin + "\n")
+    # Has version?
+    if values.version != '':
+        final_content.append("Version=" + values.version + "\n")
     # Has tryexec?
     if values.tryexec != '':
         final_content.append("TryExec=" + values.tryexec + "\n")    
     # Add exec
     final_content.append("Exec=" + values.execute + "\n")
+    # Has icon?
+    if values.icon != '':
+        final_content.append("Icon=" + values.icon + "\n")
+    # Type?
+    final_content.append("Type=Application\n")
+    # Run from terminal?
+    final_content.append("Terminal=" + values.useterm + "\n")
+    # Add category
+    final_content.append("Categories=" + values.category + "\n")
+    # Must not display?
+    if values.nodisplay != 'false':
+        final_content.append("NoDisplay=true" + "\n")
+    # Is hidden?
+    if values.hidden != 'false':
+        final_content.append("Hidden=true" + "\n")
+    # Must not show in some environment?
+    if values.notshowin != '':
+        final_content.append("NotShowIn=" + values.notshowin + "\n")
     # Has path?
     if values.path != '':
         final_content.append("Path=" + values.path + "\n")
-    # Run from terminal?
-    final_content.append("Terminal=" + values.useterm + "\n")
     # Has mime?
     if values.mime != '':
         final_content.append("MimeType=" + values.mime + "\n")
-    # Add category
-    final_content.append("Categories=" + values.category + "\n")
     # Has keywords?
     if values.keywords != '':
         final_content.append("Keywords=" + values.keywords + "\n")
     # Must notify startup?
-    final_content.append("StartupNotify=" + values.notify + "\n")
+    if values.notify != 'false':
+        final_content.append("StartupNotify=true" + "\n")
     # Has startup wm class?
     if values.wmclass != '':
         final_content.append("StartupWMClass=" + values.wmclass + "\n")
@@ -82,61 +86,71 @@ def save_type_app(values):
     # Return final content
     return final_content
 
+# Type Link
 def save_type_link(values):
     # Determine the final contents of the file
     final_content = ["[Desktop Entry]\n"]
-    # Type?
-    final_content.append("Type=Link\n")    
-    # Has version?
-    if values.version != '':
-        final_content.append("Version=" + values.version + "\n")
     # Add name
     final_content.append("Name=" + values.name + "\n")
     # Has generic name?
     if values.genericname != '':
         final_content.append("GenericName=" + values.genericname + "\n")
-    # Must not display?
-    final_content.append("NoDisplay=" + values.nodisplay + "\n")
     # Has comment?
     if values.comment != '':
         final_content.append("Comment=" + values.comment + "\n")
+    # Add URL
+    final_content.append("URL=" + values.url + "\n")
+    # Has version?
+    if values.version != '':
+        final_content.append("Version=" + values.version + "\n")
     # Has icon?
     if values.icon != '':
         final_content.append("Icon=" + values.icon + "\n")
+    # Type?
+    final_content.append("Type=Application\n")
+    # Must not display?
+    if values.nodisplay != 'false':
+        final_content.append("NoDisplay=true" + "\n")
     # Is hidden?
-    final_content.append("Hidden=" + values.hidden + "\n")
+    if values.hidden != 'false':
+        final_content.append("Hidden=true" + "\n")
     # Must not show in some environment?
     if values.notshowin != '':
         final_content.append("NotShowIn=" + values.notshowin + "\n")
-    # Add URL
-    final_content.append("URL=" + values.url + "\n")
 
     # Return final content
     return final_content
 
+# Type Directory
 def save_type_dir(values):
     # Determine the final contents of the file
-    final_content = ["[Desktop Entry]\n"]  
-    # Type?
-    final_content.append("Type=Directory\n") 
-    # Has version?
-    if values.version != '':
-        final_content.append("Version=" + values.version + "\n")
+    final_content = ["[Desktop Entry]\n"]
     # Add name
     final_content.append("Name=" + values.name + "\n")
     # Has generic name?
     if values.genericname != '':
         final_content.append("GenericName=" + values.genericname + "\n")
-    # Must not display?
-    final_content.append("NoDisplay=" + values.nodisplay + "\n")
     # Has comment?
     if values.comment != '':
         final_content.append("Comment=" + values.comment + "\n")
+    # Has version?
+    if values.version != '':
+        final_content.append("Version=" + values.version + "\n")
     # Has icon?
     if values.icon != '':
         final_content.append("Icon=" + values.icon + "\n")
+    # Type?
+    final_content.append("Type=Application\n")
+    # Run from terminal?
+    final_content.append("Terminal=" + values.useterm + "\n")
+    # Add category
+    final_content.append("Categories=" + values.category + "\n")
+    # Must not display?
+    if values.nodisplay != 'false':
+        final_content.append("NoDisplay=true" + "\n")
     # Is hidden?
-    final_content.append("Hidden=" + values.hidden + "\n")
+    if values.hidden != 'false':
+        final_content.append("Hidden=true" + "\n")
     # Must not show in some environment?
     if values.notshowin != '':
         final_content.append("NotShowIn=" + values.notshowin + "\n")
